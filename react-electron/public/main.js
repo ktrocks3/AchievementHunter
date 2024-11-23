39f53fd0240b4861a9736bc2c11c03f29251fc7c
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-11-23 11:27:59 +0100

    Added electron app

---
+++ react-electron/public/main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron')
+require('@electron/remote/main').initialize()
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webpreferences: {
+            enableRemoteModule: true
+        }
+    })
+
+    win.loadURL('http://localhost:3000')
+}
+
+app.on('ready', createWindow)
+app.on('windows-all-closed', function () {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
